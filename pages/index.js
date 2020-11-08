d0d9599b92ad46d32935c64b4eaf63bc34c18bb0
chriserl <chriserldev@gmail.com>
2020-11-08 14:13:36 +0000

    setup basic sass styling

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,10 @@ export default function Home() {
 				<title>Create Next App</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+
+			<main>
+				<p>This is the starter for this project</p>
+			</main>
 		</div>
 	);
 }
