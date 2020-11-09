6aa0ee5b405a985a9ebb8142b305f1dfe31efd1c
chriserl <chriserldev@gmail.com>
2020-11-09 12:51:55 +0000

    built basic games grid section in discover page

--- pages/views/discover/index.jsx
+++ pages/views/discover/index.jsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 import Navbar from "../../../components/Navbar/Navbar";
 import discoverStyles from "./discover.module.scss";
 
@@ -6,6 +7,44 @@ const Discover = () => {
 	let longPath = useRouter().route;
 	let shortPath = longPath.slice(7);
 	let filters = ["action", "adventure", "military", "sports", "movie"];
+	let games = [
+		{
+			gameName: "Call of Duty",
+			gameDowloads: "45k",
+			imagePath: "cod.jpg",
+			gameKey: "cod",
+		},
+		{
+			gameName: "Counter Strike",
+			gameDowloads: "10k",
+			imagePath: "cs.jpg",
+			gameKey: "cs",
+		},
+		{
+			gameName: "Fortnite",
+			gameDowloads: "150k",
+			imagePath: "fn.jpg",
+			gameKey: "fn",
+		},
+		{
+			gameName: "Ghost of Tsushima",
+			gameDowloads: "16k",
+			imagePath: "got.jpg",
+			gameKey: "got",
+		},
+		{
+			gameName: "Ghost Recon Breakpoint",
+			gameDowloads: "170k",
+			imagePath: "grb.jpg",
+			gameKey: "grb",
+		},
+		{
+			gameName: "Red Redemption Red",
+			gameDowloads: "15k",
+			imagePath: "rdr.jpg",
+			gameKey: "rdr",
+		},
+	];
 
 	return (
 		<div>
@@ -43,6 +82,30 @@ const Discover = () => {
 						))}
 					</div>
 				</div>
+
+				<section className={discoverStyles.games}>
+					<ul className={discoverStyles.gamesList}>
+						{games.map((game) => (
+							<li className={discoverStyles.gameItem} key={game.gameKey}>
+								<div className="game-card">
+									<img
+										src={`/images/games/${game.imagePath}`}
+										alt="game"
+										className="game-image"
+									/>
+									<Link href="/">
+										<a className="game-title plb">
+											{game.gameName.length > 14
+												? `${game.gameName.slice(0, 14)}...`
+												: game.gameName}
+										</a>
+									</Link>
+									<p className="game-downloads ps">{`${game.gameDowloads} Downloads`}</p>
+								</div>
+							</li>
+						))}
+					</ul>
+				</section>
 			</main>
 		</div>
 	);
