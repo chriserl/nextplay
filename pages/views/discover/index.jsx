78dcc7bf2707737b1b21c37a379c84fb1e8d7ed3
chriserl <chriserldev@gmail.com>
2020-11-09 11:23:04 +0000

    built basic filters ui

--- pages/views/discover/index.jsx
+++ pages/views/discover/index.jsx
@@ -5,25 +5,44 @@ import discoverStyles from "./discover.module.scss";
 const Discover = () => {
 	let longPath = useRouter().route;
 	let shortPath = longPath.slice(7);
+	let filters = ["action", "adventure", "military", "sports", "movie"];
 
 	return (
 		<div>
 			<Navbar activePath={shortPath} />
 
 			<main className={discoverStyles.discover}>
-				<form className={discoverStyles.headerForm}>
-					<div className="search-control">
-						<input
-							type="search"
-							name="discoverSearch"
-							className="search-input"
-							placeholder="search for games"
-						/>
-						<button className="icon-button">
-							<span className="las la-search"></span>
-						</button>
+				<div className={discoverStyles.header}>
+					<form className={discoverStyles.headerForm}>
+						<div className="search-control">
+							<input
+								type="search"
+								name="discoverSearch"
+								className="search-input"
+								placeholder="search for games"
+							/>
+							<button className="icon-button">
+								<span className="las la-search small-icon"></span>
+							</button>
+						</div>
+					</form>
+
+					<button
+						className={`light-icon-button ${discoverStyles.filterButton}`}
+					>
+						<span className="las la-sliders-h small-icon"></span>
+					</button>
+				</div>
+				<div className={discoverStyles.filters}>
+					<p className={`ph ${discoverStyles.filtersHeader}`}>Filters</p>
+					<div className={discoverStyles.filterItems}>
+						{filters.map((filterItem) => (
+							<div className={discoverStyles.filterItem} key={filterItem}>
+								<p className="tab ps">{filterItem}</p>
+							</div>
+						))}
 					</div>
-				</form>
+				</div>
 			</main>
 		</div>
 	);
