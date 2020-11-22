f6ef0bd9d364a6f9b7bae3cf3ef7169aa7317c64
chriserl <chriserldev@gmail.com>
2020-11-22 20:44:20 +0000

    wrote code to handle error in reddit post card

--- components/UIComponents/RedditCard.tsx
+++ components/UIComponents/RedditCard.tsx
@@ -1,33 +1,35 @@
 import { FunctionComponent } from "react";
 
 const RedditCard: FunctionComponent<{ postData: object }> = ({ postData }) => {
-	return (
-		<div className="reddit-card">
-			<p className="date-created pxb">{postData["created"].slice(0, 10)}</p>
-			{postData["image"] && (
-				<a href={postData["url"]}>
-					<img
-						src={postData["image"]}
-						alt={postData["text"]}
-						className="card-image"
-					/>
-				</a>
-			)}
-			<div className="card-body">
-				<div className="card-body__header">
-					<a href={postData["username_url"]} className="user-name plb">
-						{postData["name"]}
+	if (postData !== null) {
+		return (
+			<div className="reddit-card">
+				<p className="date-created pxb">{postData["created"].slice(0, 10)}</p>
+				{postData["image"] && (
+					<a href={postData["url"]}>
+						<img
+							src={postData["image"]}
+							alt={postData["text"]}
+							className="card-image"
+						/>
 					</a>
-					{/*  */}
+				)}
+				<div className="card-body">
+					<div className="card-body__header">
+						<a href={postData["username_url"]} className="user-name plb">
+							{postData["name"]}
+						</a>
+						{/*  */}
+					</div>
+					<a href={postData["url"]} className="post-text ps">
+						{postData["text"].length > 96
+							? `${postData["text"].slice(0, 148)} . . .`
+							: postData["text"]}
+					</a>
 				</div>
-				<a href={postData["url"]} className="post-text ps">
-					{postData["text"].length > 96
-						? `${postData["text"].slice(0, 148)} . . .`
-						: postData["text"]}
-				</a>
 			</div>
-		</div>
-	);
+		);
+	}
 };
 
 export default RedditCard;
