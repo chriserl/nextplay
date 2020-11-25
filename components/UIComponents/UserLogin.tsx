54dd7d96936a6f70ef2fa9be26206c7ce9a5e677
chriserl <chriserldev@gmail.com>
2020-11-25 13:55:05 +0000

    built ui for login

--- components/UIComponents/UserLogin.tsx
+++ components/UIComponents/UserLogin.tsx
@@ -1,5 +1,40 @@
 const UserLogin = ({ userAccountState, toggleAccountVisibility }) => {
-	return <div className={userAccountState}>This is the user login form</div>;
+	return (
+		<div className={userAccountState}>
+			<p className="ps form-title">Please enter your account details</p>
+			<form className="login-form">
+				<div className="search-control email-control">
+					<input
+						placeholder="Email"
+						type="email"
+						name="email"
+						id="email"
+						className="search-input"
+						autoComplete="off"
+						required
+					/>
+				</div>
+				<div className="search-control password-control">
+					<input
+						type="password"
+						name="password"
+						id="password"
+						placeholder="Password"
+						className="search-input"
+						required
+					/>
+				</div>
+				<button className="primary-button px form-submit" type="submit">
+					Login
+				</button>
+			</form>
+
+			<div className="sign-up">
+				<p className="ps">No account?</p>{" "}
+				<button className="secondary-button px sign-up-button">Sign up</button>
+			</div>
+		</div>
+	);
 };
 
 export default UserLogin;
