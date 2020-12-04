76bf1992b0c7057f3d233a38539cf2f8f50e39a9
chriserl <chriserldev@gmail.com>
2020-12-04 10:53:32 +0000

    feature: enabled toggling between create account and sign up

--- components/UIComponents/UserLogin.tsx
+++ components/UIComponents/UserLogin.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const UserLogin = ({ userAccountState, toggleAccountVisibility }) => {
-	return (
+	let [modalState, setModalState] = useState(() => ({
+		modalType: "logIn",
+	}));
+
+	const toggleModalType = () => {
+		setModalState(() =>
+			modalState.modalType === "logIn"
+				? { ...modalState, modalType: "signUp" }
+				: { ...modalState, modalType: "logIn" }
+		);
+	};
+
+	return modalState.modalType === "logIn" ? (
 		<div className={userAccountState}>
 			<span
 				onClick={() => toggleAccountVisibility()}
@@ -37,11 +51,77 @@ const UserLogin = ({ userAccountState, toggleAccountVisibility }) => {
 			</form>
 
 			<div className="sign-up">
-				<p className="ps">No account?</p>{" "}
-				<button className="secondary-button px sign-up-button">Sign up</button>
+				<p className="ps">No account?</p>
+				<button
+					onClick={() => toggleModalType()}
+					className="secondary-button px sign-up-button"
+				>
+					Sign up
+				</button>
+			</div>
+		</div>
+	) : (
+		<SignUpModal
+			toggleModalType={() => toggleModalType()}
+			userAccountState={userAccountState}
+			toggleAccountVisibility={() => toggleAccountVisibility()}
+		/>
+	);
+};
+
+function SignUpModal({
+	toggleModalType,
+	userAccountState,
+	toggleAccountVisibility,
+}) {
+	return (
+		<div className={userAccountState}>
+			<span
+				onClick={() => toggleAccountVisibility()}
+				className="md-icon secondary-tab close-icon"
+			>
+				close
+			</span>
+
+			<p className="ps form-title">Fill the form to sign up</p>
+			<form className="login-form">
+				<div className="search-control email-control">
+					<input
+						placeholder="Email"
+						type="email"
+						name="email"
+						id="email"
+						className="search-input"
+						autoComplete="off"
+						required
+					/>
+				</div>
+				<div className="search-control password-control">
+					<input
+						type="password"
+						name="password"
+						id="password"
+						placeholder="Password"
+						className="search-input"
+						required
+					/>
+				</div>
+				<button className="primary-button px form-submit" type="submit">
+					Create account
+				</button>
+			</form>
+
+			<div className="sign-up">
+				<p className="ps">Registered?</p>
+				<button
+					onClick={() => toggleModalType()}
+					className="secondary-button px sign-up-button"
+				>
+					Login
+				</button>
 			</div>
 		</div>
 	);
-};
+}
 
 export default UserLogin;
